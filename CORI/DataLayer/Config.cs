0cadc1ea095cdd8e0f868f28b2684d9f952951b9
Darko Velickovic <darko.velickovic14@gmail.com>
2015-12-26 17:11:36 +0100

    DataLayer
    Added Redis client and Config file

---
+++ CORI/DataLayer/Config.cs
@@ -0,0 +1,34 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    class Config
+    {
+        public static string SingleHost
+        {
+            get { return "localhost"; }
+        }
+        public const int RedisPort = 6379;
+        public static string SingleHostConnectionString
+        {
+            get
+            {
+                return SingleHost + ":" + RedisPort;
+            }
+        }
+        public static BasicRedisClientManager BasicClientManger
+        {
+            get
+            {
+                return new BasicRedisClientManager(new[] {
+					SingleHostConnectionString
+				});
+            }
+        }
+    }
+}
---
+++ CORI/DataLayer/DataLayer.cs
@@ -0,0 +1,23 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class DataLayer
+    {
+        readonly RedisClient redis = new RedisClient(Config.SingleHost);
+        public DataLayer()
+        {
+            test();
+        }
+        public void test()
+        {
+            redis.SetValue("test", "PeraBosko", TimeSpan.FromSeconds(20));
+            
+        }
+    }
+}
