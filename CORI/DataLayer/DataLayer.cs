fe642b429205759ca5fdd5b36f70864d922c9722
nemanjaMet <nemanja.met@gmail.com>
2016-01-08 16:41:24 +0100

    Liste

--- CORI/DataLayer/DataLayer.cs
+++ CORI/DataLayer/DataLayer.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Web;
 using ServiceStack.Text;
 using System.Runtime.Serialization;
+using System.Text.RegularExpressions; // za preciscavanje texta 'Regex'
 
 namespace DataLayer
 {
@@ -17,8 +18,21 @@ namespace DataLayer
         public DataLayer()
         {
             //test();
+
+            //--- test funkcije ---
+           // AddPerson();
+           // searchPersons();
+      
+            /*AddUser("userLista1 100");
+            AddUser("userLista1 200");
+            AddUser("userLista1 300");
+
+            AddUser("userLista2 900");
+            AddUser("userLista2 800");
+            AddUser("userLista2 700");
+            AddUser("userLista2 600");*/
         }
-        public  bool testing(int howMany)
+        public bool testing(int howMany)
         {
             string testHtml = " <div>\n\t <h1>Testing purpose</h1> \n </div>";
 
@@ -38,22 +52,42 @@ namespace DataLayer
                 return true;
         }
 
-        // --- Mislim da ne dodaje duplikate po istom id-u
-        public void AddPerson()
+       /* public void searchPersons()
         {
-            // var redisUsers = redis.As<Person>();
-            // redisUsers.GetNextSequence()
-            
-            var toske = new Person { id = 1, name = "Toske pijanac" };
-            var milojko = new Person { id = 2, name = "Milojko bre" };
-            var tripuskic = new Person { id = 3, name = "Tripuskic bez komentara" };
-            // redisUsers.Store(toske);
-            // redisUsers.Store(milojko);
+            string searchString = "*2*";
 
-            // Dodaje objekat tipa Person u bazu
-            redis.As<Person>().Store(toske);
-            redis.As<Person>().Store(milojko);
-            redis.As<Person>().Store(tripuskic);
+            var getSpecificKeys = redis.SearchKeys(searchString);
+            List<Person> listaOsoba = new List<Person>();
+          
+            // Vraca sve sta nađe string, hash, lista
+            // tako da moze da se desi da vrati neki drugi objekat
+            foreach (var getKey in getSpecificKeys)
+            {
+                // filtriranje po objektu
+                if (getKey.Contains("person"))
+                {
+                    Person per = redis.As<Person>().GetValue(getKey.ToString());
+                    listaOsoba.Add(per);
+                }
+            }
+        }*/
+
+        // Nalazi kljuceve koji sadrzi zadati podstring --- NIJE TESTIRANO ---
+        public List<string> SearchKeys(string subString)
+        {
+            string searchString = "*" + subString + "*";
+
+            var getSpecificKeys = redis.SearchKeys(searchString);
+            List<string> listaKljuceva = new List<string>();
+
+            // Vraca sve sta nađe string, hash, lista
+            // tako da moze da se desi da vrati neki drugi objekat
+            foreach (var getKey in getSpecificKeys)
+            {
+                listaKljuceva.Add(getKey.ToString());
+            }
+
+            return listaKljuceva;
         }
 
         public void AddUserHtml(string html,long UserID)
@@ -65,6 +99,12 @@ namespace DataLayer
             string javaScript1 = "";
 
             html1 = System.IO.File.ReadAllText(@"C:\Users\Darko Velickovic\Desktop\TxtHtml.txt");
+
+            // --- Za preciscavanje texta ---            
+            html1 = Regex.Replace(html1, @"\t|\r", " ");
+            html1 = Regex.Replace(html1, @"( \n){2,}", "\n");
+            html1 = Regex.Replace(html1, " {2,}", " ");
+            
 
             var stranica1 = new HtmlPage { id = UserID ,name = "stranica1", html = html1, css = css1, javaScript = javaScript1 };
            
@@ -82,60 +122,274 @@ namespace DataLayer
             return hp2.html;
         }
 
-        public void GetPerson()
+        // PushItemToList radi kao rpush
+        // Ako ne postoji takav 'nameID' liste onda pravi novu listu
+        // Ukoliko postoji takav 'nameID' liste onda ubacuje element u postojecu listu
+        public bool AddUser(string userName, string pageID)
         {
-            //---- Vraca listu objekata ----
-             IList<Person> p1 = redis.As<Person>().GetAll();
-           
-             if (p1 != null)
+            bool successfully = false;
+
+            // Ako se salje kao jedan string
+            if (!String.IsNullOrEmpty(userName) && !String.IsNullOrEmpty(pageID))
+            {
+                // Ako zadati kljuc ne postoji u bazi
+                if (!redis.ContainsKey("userLista3"))
+                {
+                    // Mozda treba da se limitira lista
+                    // Do work...
+                }
+
+                // konvertovanje pageID-a u byte array
+                byte[] idToByte = Encoding.ASCII.GetBytes(pageID);
+
+                // Ubacivanje liste u bazu, LPush ubacuje sa leve strane (kao stek, red)
+                // Vraca broj elemenata u listi, zajedno sa dodatim elementom
+                long elementsNumber = redis.LPush(userName, idToByte);
+
+                successfully = true;
+            }
+            return successfully;
+        }
+
+        // Ne znam sta da vrati listu verovatno
+        public string GetUser(string userName)
+        {
+            if (redis.ContainsKey(userName))
+            {
+                // --- Vraca sve elemente iz liste sa zadatim ID-om liste ---
+                List<string> allItemsFromList = redis.GetAllItemsFromList(userName);
+
+                // --- Ako treba sa LRANGE da se vrati ---
+                int start = 0, end = -1;
+                byte[][] byteItems = redis.LRange(userName, start, end);
+                List<string> someItemsFromList = new List<string>();
+                foreach (byte[] b in byteItems)
+                {
+                    someItemsFromList.Add(Encoding.ASCII.GetString(b));
+                }
+
+                return "---STA GOD TREBA DA VRATI---";
+            }
+            else
+                return "false";             
+        }
+
+        // Za ubacivanje elementa u list 'LPUSH'
+        public bool AddItemToList(string listID, string item)
+        {
+            bool successfully = false;
+
+            // Ako se salje kao jedan string
+            if (!String.IsNullOrEmpty(listID) && !String.IsNullOrEmpty(item))
+            {
+      
+                // konvertovanje pageID-a u byte array
+                byte[] idToByte = Encoding.ASCII.GetBytes(item);
+
+                // Ubacivanje liste u bazu, LPush ubacuje sa leve strane (kao stek, red)
+                // Vraca broj elemenata u listi, zajedno sa dodatim elementom
+                long elementsNumber = redis.LPush(listID, idToByte);
+
+                successfully = true;
+            }
+            return successfully;
+        }
+
+        // Vraca sve elemente kao lista stringova
+        public List<string> GetItemsFromList(string listID)
+        {
+            if (redis.ContainsKey(listID))
+            {
+                // --- Vraca sve elemente iz liste sa zadatim ID-om liste ---
+                List<string> allItemsFromList = redis.GetAllItemsFromList(listID);
+                return allItemsFromList;
+            }
+            else
+                return null; 
+        }
+
+        // Brise element u listu, bez obzira na kojem je mestu
+        public bool DeleteItemFromList(string listID, string itemID)
+        {
+             long successfully = redis.RemoveItemFromList(listID, itemID);
+
+             if (successfully == 1)
              {
-                 foreach (Person p in p1)
-                 {
-                     // Console.WriteLine("Id osobe: " + p.id );
-                     // Console.WriteLine("Ime osobe: " + p.name );
-                     string outputJson = ServiceStack.Text.JsonSerializer.SerializeToString<Person>(p);
-                     File.AppendAllText(@"C:\Users\Neca\Desktop\Test.txt", outputJson  + Environment.NewLine);
-                 }
+                 return true;
              }
+             else
+                 return false;
+        }
 
-            // Nacin 2 
-            // treba se pristupi podacima nekako, ili da se konvertuje u pravi objekat
-           var redisPersons = redis.As<Person>();
-           // look into : redis.GetByIds<Person>();
-          /* File.AppendAllText(@"C:\Users\Neca\Desktop\Test.txt", ???? + Environment.NewLine);*/
+        // Brise i vraca item sa pocetka liste 
+        public string PopFromStart(string listID)
+        {
+            if (redis.ContainsKey(listID))
+            {
+                byte[] byteItem = redis.LPop(listID);
 
-            //--- Vracanje osobe po ID-u ---
-             Person per1 = redis.As<Person>().GetValue("urn:person:2");
-             string toJson = ServiceStack.Text.JsonSerializer.SerializeToString<Person>(per1);
-             // ili ovako
-             var test = redis.Get<Person>("urn:person:2");
+                return Encoding.ASCII.GetString(byteItem);
 
+                // Mozda radi i ovo, ali samo brisanje (nije testirano)
+                // return redis.RemoveStartFromList(listID);
+            }
+            else
+                return "false";                
+        }
+
+        // Brise i vraca item sa kraja liste
+        public string PopFromEnd(string listID)
+        {
+            if (redis.ContainsKey(listID))
+            {
+                byte[] byteItem = redis.RPop(listID);
+
+                return Encoding.ASCII.GetString(byteItem);
+
+                // Mozda radi i ovo, ali samo brisanje (nije testirano)
+                //return redis.RemoveEndFromList(listID);
+            }
+            else
+                return "false";    
+            
+        }
+
+        // Prvo brise stari element iz liste, a zatim dodaje novi;; Na foru promenjen element je u stvari novi element
+        public bool RenameItemFromList(string listID, string oldItem, string newItem)
+        {
+            bool successfully = false;
+            if (redis.ContainsKey(listID))
+            {
+                if (DeleteItemFromList(listID, oldItem))
+                {
+                    successfully = AddItemToList(listID, newItem);
+                }
+            }
+            return successfully;
+        }
+
+        // Clear-uje celu listu ;; --- NIJE TESTIRANO ---
+        public bool ClearList(string listID)
+        {
+            // Ako zadati kljuc postoji u bazi
+            if (redis.ContainsKey(listID))
+            {
+                redis.RemoveAllFromList(listID);
+                return true;
+            }
+            else
+                return false;         
+        }
+
+        // Brise bilo sta iz redisa sto poseduje takav kljuc (lista, hash, string)
+        public bool DeleteByKey(string key)
+        {
+            return redis.Remove(key);
+        }
+
+        // --- Mislim da ne dodaje duplikate po istom id-u (radi rewrite)
+        // public void AddUser(string ID, string val)
+        // --- Dodavanje novog usera ---
+       /* public bool AddUser(string dataUser)
+        {
+            // --- STA DA SE RADI UKOLIKO POSTOJI TAKAV ID ---
+            // --- KAKO DA SE SREDI ZA ID, KAKAV DA SE UBACI ----
+
+            // Ako se salje kao jedan string
+            //string[] idValue = dataUser.Split(' ');
+            //User user = new User { id = idValue[0], value = Convert.ToInt64(idValue[1]) };
+
+            //ako se salje kao 2 stringa odvojena
+            // User user = new User { id = ID, value = Convert.ToInt64(val) };
+
+            List<long> listaValue = new List<long>();
+            listaValue.Add(1000);
+            listaValue.Add(2000);
+            listaValue.Add(3000);
+            listaValue.Add(6000);
+            listaValue.Add(5000);
+            listaValue.Add(9000);
+            listaValue.Add(7000);
+            listaValue.Add(8000);
+            listaValue.Add(1000);
+
+            User user = new User { id = "rip3", value = listaValue };
+
+            bool uspesno = redis.Set<User>("userIDTest3", user);
+
+
+            return uspesno;
+        }
+
+        public void testUser()
+        {
+            string searchString = "user*";
+
+            var getSpecificKeys = redis.SearchKeys(searchString);
+            List<User> listaOsoba = new List<User>();
+
+            // Vraca sve sta nađe string, hash, lista
+            // tako da moze da se desi da vrati neki drugi objekat
+            foreach (var getKey in getSpecificKeys)
+            {
+                // filtriranje po objektu
+                if (getKey.Contains("user"))
+                {
+                    User per = redis.As<User>().GetValue(getKey.ToString());
+                    listaOsoba.Add(per);
+                }
+            }
+        }
+
+        // --- Vracanje usera po ID-u ---
+        public string GetUser(string userId)
+        {
+            User user = redis.As<User>().GetValue("urn:user:" + userId);
+
+            if (user != null)
+            {
+                // Ako treba da se serijalizuje
+                string userJsonFormat = ServiceStack.Text.JsonSerializer.SerializeToString<User>(user);
+
+                // A ako ne treba
+                string simple = user.id + user.value.ToString();
+
+                // Vrati serijalizovani ili ne
+                return userJsonFormat;
+            }
+            else
+            {
+                // Ukoliko ne postoji
+                return "false";
+            }
+        }
+
+        // --- Brisanje usera po ID-u ---
+        public bool DeleteUser(string userId)
+        {
+            // ZA TESTIRANJE
+           bool uspesno = redis.Remove(userId);
            
-            
+            // Radi ali nema feedback od redisa
+           //redis.DeleteById<User>(userId);
+
+           return uspesno;
+ 
         }
 
-        public void DeletePerson()
+        // --- Izmena usera ---
+        public void RenameUser(string userValue)
         {
-            // --- Brisanje osobu po ID-u ---
-            redis.DeleteById<Person>("2");
-            
-            /* redis.FlushDb(); Flesuje celu bazu wtf ? // to wipe a single database, 0 by default
-             redis.FlushAll(); Flesuje celu bazu wtf ? // to wipe all databases*/
+            // --- Izmena value kod usera ---
+            // izgleda da je ovako                      
         }
 
-        public void RenamePerson()
+        public class User
         {
-            // --- Izmena imena ---
-            // izgleda da je ovako
-            var milojko = new Person { id = 2, name = "Milojko Komarac" };
-            redis.As<Person>().Store(milojko);
-        }
-
-        public class Person
-        {
-            public long id { get; set; }
-            public string name { get; set; }
-        }
+            public string id { get; set; }
+            //public long value { get; set; }
+            public List<long> value { get; set; }
+        }*/
 
         public class HtmlPage
         {
@@ -153,11 +407,6 @@ namespace DataLayer
         {
             redis.FlushAll();
         }
-
-      /*  public string ToJsonString()
-        {
-            return JsonSerializer.SerializeToString<Person>(this);
-        }*/
 
     }
 }
